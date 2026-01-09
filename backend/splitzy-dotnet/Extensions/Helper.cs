f820d26beea5c8aa6b3f22dd50d6bed68894adb1
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2026-01-10 01:25:37 +0530

    Fix : Decimal Bug Issue (#68)

---
+++ backend/splitzy-dotnet/Extensions/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace splitzy_dotnet.Extensions
+{
+    public static class Helper
+    {
+        public static decimal Normalize(decimal value)
+        {
+            return Math.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
