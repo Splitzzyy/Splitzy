3fe29350200e70b10a9b2f9f942eb37b47232ffc
singh-arekh <singharekh@gmail.com>
2026-01-11 02:52:45 +0530

    Added template for new user asked to join group

--- backend/splitzy-dotnet/DTO/EmailDTO.cs
+++ backend/splitzy-dotnet/DTO/EmailDTO.cs
@@ -13,4 +13,6 @@ namespace splitzy_dotnet.DTO
     public record GroupAddedRequest(string To, string UserName, string GroupName, string AddedBy);
 
     public record ForgotPasswordRequest(string To, string ResetLink);
+
+    public record GroupInvitationRequest(string To, string InviterName, string GroupName);
 }
