0c0f66e6c8220300fe02fec53f43f8f3443d5180
shubhamashish33 <shubhamashish8@gmail.com>
2026-01-27 22:41:22 +0530

    removed unused imports

--- UI/src/app/splitz/verify-email/verify-email.component.ts
+++ UI/src/app/splitz/verify-email/verify-email.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { LoaderComponent } from '../loader/loader.component';
 import { SplitzService } from '../services/splitz.service';
